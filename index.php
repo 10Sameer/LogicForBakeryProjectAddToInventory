48ab5110fdc248d8758a90619a14d8cebe3e88e0
Sameer <bhandarisameer173@gmail.com>
2025-03-11 21:09:00 +0545

    Admin Panel CSS

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_POST['login'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Login</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="index.css">
 </head>
 <body>
   <div class="login-container">
