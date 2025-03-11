b47c082a0cf0025d0fd6876e359676de4492cba3
Sameer <bhandarisameer173@gmail.com>
2025-03-11 21:10:37 +0545

    Admin Dashboard

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION['userid'])) {
+    header('Location: index.php'); 
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="dashboard-container">
+    <div class="glass-form">
+      <h1>Welcome to Admin Dashboard</h1>
+      <a href="addProduct.php"><button id="addToInventory">Add to Inventory</button></a>
+    </div>
+  </div>
+</body>
+</html>
